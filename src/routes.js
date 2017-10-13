c7bba59e8fb3ace86df242dc279468eaa2e00984
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2017-10-13 19:48:39 -0300

    Remove hello.js routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const KoaRouter = require('koa-router');
 
-const hello = require('./routes/hello');
 const index = require('./routes/index');
 const session = require('./routes/session');
 const users = require('./routes/users');
@@ -10,7 +9,6 @@ const admin = require('./routes/admin');
 const router = new KoaRouter();
 
 router.use('/', index.routes());
-router.use('/hello', hello.routes());
 router.use('/session', session.routes());
 router.use('/admin', admin.routes());
 router.use('/user', users.routes());
